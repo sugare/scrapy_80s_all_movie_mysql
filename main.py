e2a0d7d8a2a1933567d3fea08aa642cf806af806
sugare <30733705@qq.com>
2016-10-20 09:16:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import MySQLdb
 
 conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="1234")
 cur = conn.cursor()
-cur.execute('create database if not exists python')
-conn.select_db('python')
-cur.execute('create table student(`name` char(20) not null,`link` char(20) not null,`desc` varchar(16) default null)')
+cur.execute('create database if not exists scrapydb')
+conn.select_db('scrapydb')
+cur.execute('create table tb_scrapy(`name` char(20) not null,`link` char(20) not null,`desc` varchar(16) default null)')
 conn.commit()
 cur.close()
 conn.close()
