16afb8a97348150daaad7e34c2ebe367e5070fab
sugare <30733705@qq.com>
2016-10-20 09:15:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from scrapy import cmdline
 #cmdline.execute("scrapy crawl movies".split())
 #cmdline.execute("scrapy crawl movies_recursion".split())
+import MySQLdb
+
+conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="1234")
+cur = conn.cursor()
+cur.execute('create database if not exists python')
+conn.select_db('python')
+cur.execute('create table student(`name` char(20) not null,`link` char(20) not null,`desc` varchar(16) default null)')
+conn.commit()
+cur.close()
+conn.close()
+
 cmdline.execute("scrapy crawl movies_recursion_mysql".split())
