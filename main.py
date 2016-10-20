495bca06877b458883bb7bbc9f08e056c97a0129
sugare <30733705@qq.com>
2016-10-20 08:26:25 +0800

    -A

---
+++ main.py
@@ -0,0 +1,4 @@
+from scrapy import cmdline
+#cmdline.execute("scrapy crawl movies".split())
+#cmdline.execute("scrapy crawl movies_recursion".split())
+cmdline.execute("scrapy crawl movies_recursion_mysql".split())
